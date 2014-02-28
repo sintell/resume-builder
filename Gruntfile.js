f57b91ee417874c8902a895c01b3793120e41c67
sintell <uyiiio@gmail.com>
2014-02-28 17:53:15 +0400

    Установлен grunt, создан default task

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    base: ".",
+                    keepalive: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    // Default task(s).
+    grunt.registerTask('server', 'Start a custom web server', function() {
+        grunt.log.writeln('Started web server on port 3000');
+        require('./server/app.js').listen(3000);
+    });
+    grunt.registerTask('default', ['server','connect']);
+
+};

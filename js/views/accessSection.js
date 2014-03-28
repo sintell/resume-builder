46feedbd94e31fa8650f7e19bb5bc7409323e3e1
Ilya Khokhryakov <ilyahohryakov@gmail.com>
2014-03-28 19:12:00 +0400

    Разделил основной шаблон резюме и вынес блоки в отдельные view

---
+++ js/views/accessSection.js
@@ -0,0 +1,43 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'views/resumeSection',
+    'views/accessType',
+    'views/accessTypeCompanyList',
+    'text!templates/accessSection.html'
+], function(
+    $,
+    _,
+    Backbone,
+    ResumeSection,
+    AccessTypeView,
+    AccessTypeCompanyListModal,
+    AccessSectionTemplate
+) {
+    'use strict';
+
+    return ResumeSection.extend({
+        className: 'HH-Resume-ResumeSection HH-Resume-ResumeSectionAccess',
+
+        namespace: 'access',
+
+        template: _.template(AccessSectionTemplate),
+
+        initialize: function(options) {
+            var modal;
+
+            ResumeSection.prototype.initialize.apply(this, [options]);
+
+            $.extend(options, {resume: this.model});
+
+            modal = new AccessTypeCompanyListModal();
+            this.components.push(modal);
+            this.components.push(new AccessTypeView(options, modal));
+        },
+
+        render: function(data) {
+            return ResumeSection.prototype.render.apply(this, arguments);
+        }
+    });
+});

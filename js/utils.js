d1da3ce2c1db813002e08f9ecc1aaf74f1a100b9
Tims101 <tims101@yandex.ru>
2014-04-04 17:34:56 +0400

    Добавлены названия месяцев, исправлены падежи во времени

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,12 @@
-define(["underscore"],function(_) {
+define(['underscore'],function(_) {
    var Utils = function() {
-       // только для lowercase
+       // Данный метот применим только для входного текста в нижнем регистре,
+       // т.к. символ '`' при нажатом капс-логе выглядит и одинаково.
+       // В итоге, при написании слова RJHJK`D - (КОРОЛЁВ) мы
+       // не можем определить, какая буква ё в строке, большая или маленькая.
+       // И, чтобы быть честым, мы гарантируем корректность выполнения только
+       // дя входного текста в нижнем регистре.
+       // При этом, функция не заменяет символы, которым нет соответствия на русской клавиатуре.
        this.toRussianKeyboard = function(text) {
            var keyboard = {
                'q' : 'й',
@@ -54,13 +60,18 @@ define(["underscore"],function(_) {
            var MINUTE = SECOND * 60;
            var HOUR = MINUTE * 60;
            var DAY = HOUR * 24;
-           var dateString = "";
+           var dateString = '';
 
            // TODO: что будет, если у пользователя некорректно проставлено время?
            var now = new Date();
            var diff = now - date;
 
-           if (diff < 60 * SECOND) {
+           if (diff < 0) {
+               return [
+                   [date.getDate(), this.monthNameByNum(date.getMonth()), date.getFullYear()].join(' '),
+                   [date.getHours(), date.getMinutes()].join(':')
+               ].join(' ');
+           } else if (diff < 60 * SECOND) {
                var seconds = Math.round(diff/SECOND);
                dateString = [seconds, this.secondsToRussian(seconds), 'назад'].join(' ');
            } else if (diff < 60 * MINUTE) {
@@ -69,9 +80,11 @@ define(["underscore"],function(_) {
            } else if (diff < 24 * HOUR) {
                var hours = Math.round(diff/HOUR);
                dateString = [hours, this.hoursToRussian(hours), 'назад'].join(' ');
-           } else {
+           } else if (diff < 30 * DAY) {
                var days = Math.round(diff/DAY);
                dateString = [days, this.daysToRussian(days), 'назад'].join(' ');
+           } else {
+               return [date.getDate(), this.monthNameByNum(date.getMonth()), date.getFullYear()].join(' ');
            }
 
            return dateString;
@@ -93,32 +106,49 @@ define(["underscore"],function(_) {
        // использование: numeralToRussian(число обьектов, массив значений строки для 1, 4 и 5 обьектов)
        this.numeralToRussian = function(number, titles) {
            var cases = [2, 0, 1, 1, 1, 2];
-           return titles[(number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
+           return titles[((number % 100 > 4) && (number % 100 < 20)) ? 2 : cases[(number % 10 < 5) ? (number % 10) : 5]];
        };
 
        this.secondsToRussian = function(val) {
-            return this.numeralToRussian(val, ["секунда", "секунды", "секунд"]);
+            return this.numeralToRussian(val, ['секунду', 'секунды', 'секунд']);
        };
 
        this.minutesToRussian = function(val) {
-           return this.numeralToRussian(val, ["минута", "минуты", "минут"]);
+           return this.numeralToRussian(val, ['минуту', 'минуты', 'минут']);
        };
 
        this.hoursToRussian = function(val) {
-           return this.numeralToRussian(val, ["час", "часа", "часов"]);
+           return this.numeralToRussian(val, ['час', 'часа', 'часов']);
        };
 
        this.daysToRussian = function(val) {
-           return this.numeralToRussian(val, ["день", "дня", "дней"]);
+           return this.numeralToRussian(val, ['день', 'дня', 'дней']);
        };
 
        this.monthsToRussian = function(val) {
-           return this.numeralToRussian(val, ["месяц", "месяца", "месяцев"]);
+           return this.numeralToRussian(val, ['месяц', 'месяца', 'месяцев']);
        };
 
        this.isIOS = function() {
            return /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
        }
+
+       this.monthNameByNum = function(val) {
+            var dict = [
+                'января',
+                'февраля',
+                'марта',
+                'апреля',
+                'мая',
+                'июня',
+                'июля',
+                'августа',
+                'сентября',
+                'октября',
+                'декабря'];
+
+           return dict[val];
+       };
    };
 
     return new Utils();

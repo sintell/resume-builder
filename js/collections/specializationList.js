794899ef51936300f07f4d67ef8b17e93b57d49e
Ilya Khokhryakov <ilyahohryakov@gmail.com>
2014-04-13 01:29:49 +0400

    Удалил ненужную модель

--- js/collections/specializationList.js
+++ js/collections/specializationList.js
@@ -1,13 +1,14 @@
 define([
-    'underscore',
     'backbone',
-    'models/specialization',
     'config'
-], function(_, Backbone, Specialization, Config) {
+], function(
+    Backbone,
+    Config
+) {
     'use strict';
 
     return Backbone.Collection.extend({
-        model: Specialization,
+        model: Backbone.Model,
 
         url: [Config.apiUrl, 'specializations'].join('/')
     });
